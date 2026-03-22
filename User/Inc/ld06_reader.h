055076db78e5e5c74be75b1bc9650dabbf021fb2
BOX268 <119125853+BOX268@users.noreply.github.com>
2026-03-22 14:41:12 +0100

    - added some infos about the data frame, that I suppose are correct

--- User/Inc/ld06_reader.h
+++ User/Inc/ld06_reader.h
@@ -1,11 +1,29 @@
 # ifndef __LD06_READER_H
 # define __LD06_READER_H
 
+/**
+ * 
+ * @note LD06 data frame format from https://gibbard.me/lidar/ :
+ * 
+ * <header_byte>
+ * <length (uint8)>
+ * <speed (uint16)>
+ * <start_angle (uint16)>
+ * then, the measurements :
+ * <length (uint16)> + <confidence (uint8)> * LD06_MEASUREMENT_AMOUNT
+ * <stop_angle (uint16)>
+ * <timestamp (uint16)>
+ * <crc (uint8)>
+ * 
+ * the data is in little endian, for example 1023 is sent as 0xff 0x03
+ */
+
 # include "usart.h"
 # include <inttypes.h>
 
 # define LD06_HEADER_BYTE 0x54
 # define LD06_PACKET_SIZE 47
+# define LD06_MEASUREMENT_AMOUNT 12
 
 # define LD06_UART huart2
 
